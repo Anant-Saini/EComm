087ade4d4f46f94442cc5c9d3c2627054edce625
Anant Saini <anant-saini@Anants-MacBook-Air.local>
2026-03-22 20:15:02 +0530

    setup project structure

---
+++ src/main/java/org/arth/ecomm/ECommApplication.java
@@ -0,0 +1,15 @@
+package org.arth.ecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ECommApplication.class, args);
+        System.out.println("Hello Anant!");
+    }
+
+}
---
+++ src/test/java/org/arth/ecomm/ECommApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arth.ecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
